ee4293d359cf25b508483fe5fbef4f6894bb6cb8
JosueAFC64 <josueadrianfranco@gmail.com>
2025-11-24 20:32:55 -0500

    Feature: microservicio Medicamentos

---
+++ ApiMedicamentos/src/main/java/cm/apimedicamentos/dto/MedicamentosResponse.java
@@ -0,0 +1,15 @@
+package cm.apimedicamentos.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record MedicamentosResponse(
+        @Schema(description = "Identificador único del Medicamento", example = "1")
+        Long id,
+
+        @Schema(description = "Nombre del Medicamento", example = "Amoxicilina")
+        String nombre,
+
+        @Schema(description = "Presentación del Medicamento", example = "Cápsulas de 500 mg")
+        String presentacion
+) {
+}
